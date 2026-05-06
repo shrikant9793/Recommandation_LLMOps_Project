c8d6d896384c56632b1abf9c57640d0ac2a4975d
shrik9793 <shrinaths2@gmail.com>
2026-05-07 00:49:43 +0530

    added data with data_loader

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+    setup(
+        name="recommandation_llmops_project",
+        version="0.1.0",
+        packages=find_packages(),
+        install_requires=requirements,
+    )
+
+    
